791dfe903d627ccf44f25f8d1400be74a850a074
bIDDERRR <88043774+bIDDERRR@users.noreply.github.com>
2023-07-26 01:34:11 -0300

    pagina de materias quase concluida

---
+++ app.js
@@ -0,0 +1,22 @@
+const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require ("body-parser")
+
+const _ = require('lodash');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(3000, function(req, res){
+    console.log("O servidor está funcionando e ativo.")
+})
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+app.get("/compose", function(req, res){
+    res.render("compose")
+})
