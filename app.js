e035d25bac12b3478e71b4e5fe7a6585b58f3430
bIDDERRR <88043774+bIDDERRR@users.noreply.github.com>
2023-08-30 21:49:07 -0300

    passagem de dados e rota de card para pagina

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const extremamente = "Extremamente dificil";
 const umPouco = "Um pouco dificil";
 const nemUmPouco = "Nem um pouco dificil";
 
-let subjects = []
+let subjects = [];
+let Topics = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,6 +21,10 @@ app.listen(3000, function(req, res){
     console.log("O servidor está funcionando e ativo.")
 })
 
+app.get('/signatures', function(req, res){
+    res.render('signatures')
+})
+
 app.get("/", function(req, res){
     res.render("home", {newSubject: subjects})
 })
@@ -27,8 +32,26 @@ app.get("/compose", function(req, res){
     res.render("compose")
 })
 
-app.get("/subject", function(req, res){
-    res.render("subject", {newListItems: items});
+app.get("/subject/:subjectName", function(req, res){
+    let parameter = req.params.subjectName;
+    const kebabParam = _.kebabCase(parameter)
+    parameter = kebabParam;
+
+    subjects.forEach(subject =>{
+         let subjTitle = _.kebabCase(subject.title)
+         if(subjTitle === parameter){
+            
+           res.render('subject', {subjectTitle: subject.title,
+             subjectSelector: subject.selector, subjDesc: subject.description
+             }) 
+        }
+         else{
+            console.log("It has ocurred an error.")
+        }
+    })
+
+
+    res.render("subject" )
 })
 
 app.post("/compose", function(req, res){
