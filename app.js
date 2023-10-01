23ec2e9f7fc8a7504bcaa403949bcfd520d4b51e
bIDDERRR <88043774+bIDDERRR@users.noreply.github.com>
2023-10-01 02:53:35 -0300

    passagem de dados entre todas as paginas feitas

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.listen(3000, function (req, res) {
     console.log("O servidor está funcionando e ativo.")
 })
 
-app.get('/signatures', function (req, res) {
-    res.render('signatures')
-})
- 
 app.get("/", function (req, res) {
     res.render("home", { newSubject: subjects })
 })
@@ -27,18 +23,6 @@ app.get("/", function (req, res) {
 app.get("/compose", function (req, res) {
     res.render("compose")
 
-})
-
-app.get("/progress", function(req, res){
-    res.send("A Pagina não esta pronta ainda :/")
-})
-
-app.get("/tips", function(req, res){
-    res.render("tips")
-})
-
-app.get("/accounts", function(req, res){
-    res.render("accounts")
 })
 
 app.get("/subject/:subjectName", function (req, res) {
@@ -52,7 +36,8 @@ app.get("/subject/:subjectName", function (req, res) {
 
             res.render('subject', {
                 subjectTitle: subject.title,
-                subjectSelector: subject.selector, subjDesc: subject.description
+                subjectSelector: subject.selector, subjDesc: subject.description, 
+                subjectTasks: subject.tasks
             })
         }
         else {
@@ -68,21 +53,51 @@ app.get("/subject/:subjectName", function (req, res) {
 
 app.post("/compose", function (req, res) {
 
-    const data =req.body;
+    const data =req.body;   
+   
     
     const subjectData = {
         title: data.subjectTitle, 
         selector :data.subjectSelector,
         description : data.subjectDesc,
         tasks: data.tasks
+    } 
+    /* const selector = data.subjectSelector; */
+   /*  if(selector  === 1){
+        selector = 'extremamente dificl'
     }
-   
-   
+    if(selector ===2){
+        selector ='um pouco dificil'
+    }
+    if(selector===3 ){
+       selector = 'nem um pouco dificil'
+    } */
+    /* console.log(selector) */
     subjects.push(subjectData)
     console.log(subjects)
    
 })
 
- /* console.log('Dados da matéria recebidos:', subjectData); */
-    /* passando dados da pagina de compose para o servidor 
-    e retornando eles como um card na home */
+//paginas estaticas
+
+app.get('/signatures', function (req, res) {
+    res.render('signatures')
+})
+app.get("/progress", function(req, res){
+    res.send("A Pagina não esta pronta ainda :/")
+})
+
+app.get("/tips", function(req, res){
+    res.render("tips")
+})
+
+app.get("/accounts", function(req, res){
+    res.render("accounts")
+})
+
+
+
+
+// TODO criar um novo object com o nome de selector
+//onde o key value pair selector:data.subjectSelector ira receber os dados do seletor que estao na const Data normalmente
+//depois ira passar esses dados diretamente para subjectData apos terem sido julgados por uma if condition
